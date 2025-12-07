e365738b6045a14e2be20738f5d39af1e6babcec
23050155-sketch <23050155@student.bdu.edu.vn>
2025-12-07 16:15:59 +0000

    cap nhat

--- app/main.py
+++ app/main.py
@@ -6,30 +6,18 @@ from app.models.leave_request import LeaveRequest
 from app.models.payroll import Payroll
 from app.routers.stats import router as stats_router
 from app.routers import reports
-from app.models.user import User
  
 from app.routers.employee import router as employee_router
 from app.routers.attendance import router as attendance_router
 from app.routers.leave_request import router as leave_router
 from app.routers.payroll import router as payroll_router
-from app.routers.auth import router as auth_router
 
 # Tạo bảng trong MySQL nếu chưa có
 Base.metadata.create_all(bind=engine)
 
-from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
-
-app = FastAPI(
-    title="HR Employee Management",
-    swagger_ui_init_oauth={
-        "usePkceWithAuthorizationCodeGrant": True
-    }
-)
-
+app = FastAPI(title="HR Employee Management")
 
 # 🔥 QUAN TRỌNG: gắn router Employees vào app
-app.include_router(auth_router)
 app.include_router(employee_router)
 app.include_router(attendance_router)
 app.include_router(leave_router)
@@ -40,40 +28,3 @@ app.include_router(reports.router)
 @app.get("/")
 def root():
     return {"status": "OK", "message": "HR API is running"}
-
-
-
-
-
-from fastapi.openapi.utils import get_openapi
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-
-    openapi_schema = get_openapi(
-        title="HR Employee Management",
-        version="1.0",
-        description="HR System",
-        routes=app.routes,
-    )
-
-    openapi_schema["components"]["securitySchemes"] = {
-        "BearerAuth": {
-            "type": "http",
-            "scheme": "bearer",
-            "bearerFormat": "JWT",
-        }
-    }
-
-    for path in openapi_schema["paths"]:
-        for method in openapi_schema["paths"][path]:
-            openapi_schema["paths"][path][method]["security"] = [
-                {"BearerAuth": []}
-            ]
-
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-app.openapi = custom_openapi
-
--- app/routers/employee.py
+++ app/routers/employee.py
@@ -3,18 +3,12 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.database import get_db
-from app.core.security import get_current_user, get_current_admin
-
 from app.models.employee import Employee
 from app.schemas.employee import EmployeeCreate, EmployeeUpdate, EmployeeOut
 
 router = APIRouter(prefix="/employees", tags=["Employees"])
 
-@router.post(
-    "/", 
-    response_model=EmployeeOut,
-    dependencies=[Depends(get_current_admin)]
-    )
+@router.post("/", response_model=EmployeeOut)
 def create_employee(emp: EmployeeCreate, db: Session = Depends(get_db)):
     new_emp = Employee(**emp.dict())
     db.add(new_emp)
@@ -23,10 +17,7 @@ def create_employee(emp: EmployeeCreate, db: Session = Depends(get_db)):
     return new_emp
 
 @router.get("/", response_model=List[EmployeeOut])
-def get_employees(
-    db: Session = Depends(get_db),
-    current_user = Depends(get_current_user),
-    ):
+def get_employees(db: Session = Depends(get_db)):
     return db.query(Employee).all()
 
 @router.get("/{emp_id}", response_model=EmployeeOut)
